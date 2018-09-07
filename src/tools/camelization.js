da8eef337cd3b099ab6ed39e4496e7b44e8f50d5
shijinyu <jinyu.shi@ideacome.com>
2018-09-07 14:31:14 +0800

    modify

---
+++ src/tools/camelization.js
@@ -0,0 +1,15 @@
+// Matches dashed string for camelizing
+const rmsPrefix = /^-ms-/;
+const rdashAlpha = /-([a-z])/g;
+
+// Used by camelCase as callback to replace()
+function fcamelCase(all, letter) {
+  return letter.toUpperCase();
+}
+
+// Convert dashed to camelCase; used by the css and data modules
+// Support: IE <=9 - 11, Edge 12 - 15
+// Microsoft forgot to hump their vendor prefix (#9572)
+export default function camelCase(str) {
+  return str.replace(rmsPrefix, 'ms-').replace(rdashAlpha, fcamelCase);
+}
